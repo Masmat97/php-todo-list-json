907e3cfaef7a63897ae45bc8587520d5d60368c1
Masmat97 <matteo.mascio@libero.it>
2024-06-17 15:29:01 +0200

    M1

--- api-client/main.js
+++ api-client/main.js
@@ -3,15 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            friends: []
+            friends: [],
+            newTask: ""
         }
     },
     methods: {
+        addTask() {
+            console.log("aggiungi", this.newTask);
 
+            const newFriend = {
+                name: this.newTask,
+                last_name: "Cognome"
+            };
+            this.friends.push(newFriend)
+        }
     },
     mounted() {
         axios.get("../server.php").then(results => {
-            console.log("Risultati:" , results);
+            console.log("Risultati:", results);
             this.friends = results.data;
         });
 
--- server.php
+++ server.php
@@ -1,29 +1,10 @@
 <?php
 
-$friends = [
-     [
-        'name' => 'Luca',
-        'last_name' => 'Rossi'
-     ],
-     [
-        'name' => 'Antonio',
-        'last_name' => 'Bianchi'
-     ],
-     [
-        'name' => 'Giuseppe',
-        'last_name' => 'Verdi'
-     ],
-     [
-        'name' => 'Cristoforo',
-        'last_name' => 'Colombo'
-     ],
-     [
-        'name' => 'Simone',
-        'last_name' => 'Esposito'
-     ],
-];
+$fileContent = file_get_contents("dati.json");
+
 header('Content-Type: application/json');
 
-$jsonString = json_encode($friends);
 
-echo $jsonString;
+echo $fileContent;
+
+
